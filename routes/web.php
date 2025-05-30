8c250d2078bb06deea339cdbf8da80a34590b04b
Wajid <wwajidali@gmail.com>
2025-05-30 15:26:09 +0500

    auth routes

--- routes/web.php
+++ routes/web.php
@@ -22,16 +22,19 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/inventory', [App\Http\Controllers\Api\InventoryController::class, 'index'])->name('inventory.index');
-Route::patch('/inventory/{id}', [App\Http\Controllers\Api\InventoryController::class, 'update'])->name('inventory.update');
-Route::post('/inventory', [App\Http\Controllers\Api\InventoryController::class, 'store'])->name('inventory.store');
+Route::middleware(['auth'])->group(function ()
+{
+    Route::get('/inventory', [App\Http\Controllers\Api\InventoryController::class, 'index'])->name('inventory.index');
+    Route::patch('/inventory/{id}', [App\Http\Controllers\Api\InventoryController::class, 'update'])->name('inventory.update');
+    Route::post('/inventory', [App\Http\Controllers\Api\InventoryController::class, 'store'])->name('inventory.store');
 
-Route::get('/pricing', [App\Http\Controllers\Api\PricingController::class, 'index'])->name('pricing.index');
-Route::post('/pricing', [App\Http\Controllers\Api\PricingController::class, 'store'])->name('pricing.store');
+    Route::get('/pricing', [App\Http\Controllers\Api\PricingController::class, 'index'])->name('pricing.index');
+    Route::post('/pricing', [App\Http\Controllers\Api\PricingController::class, 'store'])->name('pricing.store');
 
-Route::get('/transactions', [App\Http\Controllers\Api\TransactionController::class, 'index'])->name('transactions.index');
-Route::post('/transactions', [App\Http\Controllers\Api\TransactionController::class, 'store'])->name('transactions.store');
+    Route::get('/transactions', [App\Http\Controllers\Api\TransactionController::class, 'index'])->name('transactions.index');
+    Route::post('/transactions', [App\Http\Controllers\Api\TransactionController::class, 'store'])->name('transactions.store');
 
-Route::get('/audit-logs', [App\Http\Controllers\Api\AuditLogController::class, 'index'])->name('audit_logs.index');
+    Route::get('/audit-logs', [App\Http\Controllers\Api\AuditLogController::class, 'index'])->name('audit_logs.index');
 
-Route::resource('products', App\Http\Controllers\Api\ProductController::class);
+    Route::resource('products', App\Http\Controllers\Api\ProductController::class);
+});
